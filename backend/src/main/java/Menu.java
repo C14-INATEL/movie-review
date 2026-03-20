894d1ef748182d2692cadc60e7588e26c7344c81
Vitoria Bernardo <cassiavitoriaa2@gmail.com>
2026-03-20 14:45:12 -0300

    feat: implementa interface inicial do menu

---
+++ backend/src/main/java/Menu.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner leitor = new Scanner(System.in);
+
+    public void exibirOpcoes() {
+        System.out.println("--- SISTEMA DE FILMES (GRUPO C14) ---");
+        System.out.println("1 - Listar Catálogo");
+        System.out.println("2 - Cadastrar Usuário");
+        System.out.println("3 - Avaliar um Filme");
+        System.out.println("4 - Ver Ranking de Notas");
+        System.out.println("0 - Sair");
+        System.out.print("Digite a opção: ");
+        
+        int escolha = leitor.nextInt();
+        System.out.println("Opção " + escolha + " selecionada. (Em desenvolvimento)");
+    }
+}
